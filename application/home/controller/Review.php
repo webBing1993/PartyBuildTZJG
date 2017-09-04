2a0d6f4e29900cf86b21e345ba58b55db5cd3ad4
wzc <958364865@qq.com>
2017-09-04 09:51:34 +0800

    消息审核  模块 修改

--- application/home/controller/Review.php
+++ application/home/controller/Review.php
@@ -307,31 +307,14 @@ class Review extends Base{
      */
     public function push(){
         $msg = input('post.');
-        $push = Push::where('id',$msg['id'])->find();
-        $this_info['status'] = $msg['status'];   //更新push表状态
-        if($msg['status'] == 3 ){  // 审核通过
-            // 发送消息
-            $arr1 = $push['focus_main'];    //主图文id
-            !empty($push['focus_vice']) ? $arr2 = json_decode($push['focus_vice']) : $arr2 = "";    //副图文id
-            //主图文信息  根据 class 的值 获取不同的数据
-            switch ($push['class']){
-                case 1:
-                    $res = $this->push_detail(1,$arr1,$arr2);
-                    break;
-                case 2:
-                    $res = $this->push_detail(2,$arr1,$arr2);
-                    break;
-                case  3:
-                    $res = $this->push_detail(3,$arr1,$arr2);
-                    break;
-            }
-            if ($res['errcode'] == 0) {
-                Push::where('id',$msg['id'])->update($this_info);    //改变推送状态
-                return $this->success("审核通过，已推送消息");
-            };
-        }else{  //不通过
-            Push::where('id',$msg['id'])->update($this_info);
-            return $this->error("审核不通过");
+        if($msg['status'] == 3 ){  // 审核通过  推送消息
+            $this->push_detail($msg['class'],$msg['id']);
+        }
+        $res = $this->change_status($msg['class'],$msg['id'],$msg['status']);
+        if ($res){
+            return $this->success('审核成功');
+        }else{
+            return $this->error('审核失败');
         }
     }
     /**
@@ -373,151 +356,87 @@ class Review extends Base{
     }
     /**
      * 获取推送详情
+     * 党建责任 responsibility  两学一做 learn 组织建设 organization 特色创新 special 作风建设 style 志愿服务 volunteer 党风廉政 incorrupt
      */
-    public function push_detail($type,$main,$voice){
+    public function push_detail($type,$main){
         switch ($type) {    //根据类别获取表明
             case 1:
-                $table = "work";
-                $pre = "【第一聚焦】";
-                $url = "news/detail";
-                $agentid = '';
+                $table = "responsibility";
+                $pre = "【党建责任】";
+                $url = "Responsibility/detail";
+                $agentid = 1000002;
                 break;
             case 2:
-                $table = "centraltask";
-                $pre = "【第一聚焦】";
-                $url = "news/detail";
-                $agentid = '';
+                $table = "learn";
+                $pre = "【两学一做】";
+                $url = "Learn/detail";
+                $agentid = 1000003;
                 break;
             case 3:
-                $table = "policy";
-                $pre = "【第一聚焦】";
-                $url = "news/detail";
-                $agentid = '';
+                $table = "organization";
+                $pre = "【组织建设】";
+                $url = "Organization/detail";
+                $agentid = 1000004;
                 break;
             case 4:
-                $table = "learn";
-                $pre = "【第一聚焦】";
-                $url = "news/detail";
-                $agentid = '';
+                $table = "special";
+                $pre = "【特色创新】";
+                $url = "Special/detail";
+                $agentid = 1000005;
                 break;
             case 5:
-                $table = "news";
-                $pre = "【第一聚焦】";
-                $url = "news/detail";
+                $table = "style";
+                $pre = "【作风建设】";
+                $url = "Style/detail";
                 $agentid = '';
+                break;
+            case 6:
+                $table = "volunteer";
+                $pre = "【志愿服务】";
+                $url = "Volunteer/detail";
+                $agentid = 1000006;
+                break;
+            case 7:
+                $table = "incorrupt";
+                $pre = "【党风廉政】";
+                $url = "Incorrupt/detail";
+                $agentid = 1000007;
                 break;
             default:
                 return $this->error("无该数据表");
                 break;
         }
         //活动基本信息  主图文
-        $focus1 = Db::name($table)->find(['id' => $main]);
+        $focus = Db::name($table)->find(['id' => $main]);
         if (empty($list)){
             $this ->error('该内容不存在或已删除!');
         }
-        $title1 = $focus1['title'];
-        $str1 = strip_tags($focus1['content']);
-        $des1 = mb_substr($str1, 0, 40);
-        $content1 = str_replace("&nbsp;", "", $des1);  //空格符替换成空
-        $url1 = Config::get('host_url')."/home/".$url."/id/" . $focus1['id'] . ".html";
-        $img1 = Picture::get($focus1['front_cover']);
-        $path1 = Config::get('host_url') . $img1['path'];
-        $information1 = array(
-            "title" => $pre . $title1,
-            "description" => $content1,
-            "url" => $url1,
-            "picurl" => $path1,
+        $title = $focus['title'];
+        $str = strip_tags($focus['content']);
+        $des = mb_substr($str, 0, 40);
+        $content = str_replace("&nbsp;", "", $des);  //空格符替换成空
+        $url = Config::get('host_url')."/home/".$url."/id/" . $focus['id'] . ".html";
+        $img = Picture::get($focus['front_cover']);
+        $path = Config::get('host_url') . $img['path'];
+        $send = array(
+            "articles" => array(
+                "title" => $pre . $title,
+                "description" => $content,
+                "url" => $url,
+                "picurl" => $path,
+            )
         );
-        $information = array();
-        if (!empty($voice)) {
-            //副图文信息
-            $information2 = array();
-            foreach ($voice as $key => $value) {
-                $focus = Db::name($table)->find(['id' => $value]);
-                $title = $focus['title'];
-                $str = strip_tags($focus['content']);
-                $des = mb_substr($str, 0, 40);
-                $content = str_replace("&nbsp;", "", $des);  //空格符替换成空
-                $url = Config::get('host_url')."/home/".$url."/id/" . $focus['id'] . ".html";
-                $img = Picture::get($focus['front_cover']);
-                $path = Config::get('host_url') . $img['path'];
-                $info = array(
-                    "title" => $pre. $title,
-                    "description" => $content,
-                    "url" => $url,
-                    "picurl" => $path,
-                );
-                $information2[] = $info;
-            }
-            //数组合并，主图文放在首位
-            foreach ($information2 as $k => $v) {
-                $information[0] = $information1;
-                $information[$k + 1] = $v;
-            }
-        } else {
-            $information[0] = $information1;
-        }
-        //重组成article数据
-        $send = array();
-        $re[] = $information;
-        foreach ($re as $key => $value) {
-            $key = "articles";
-            $send[$key] = $value;
-        }
         //发送给服务号
         $Wechat = new TPQYWechat(Config::get('news'));
         $message = array(
-//                   'totag' => "4", //审核标签用户
-            'touser' =>'15036667391',
+            'touser' =>'17557289172',
 //                   "touser" => "@all",   //发送给全体，@all
             "msgtype" => 'news',
-            "agentid" =>$agentid,  // 小镇动态
+            "agentid" =>$agentid,
             "news" => $send,
             "safe" => "0"
         );
         $suc = $Wechat->sendMessage($message);
         return $suc;
-    }
-    /**
-     * 获取 待审核  列表
-     */
-    public function get_list($status=0,$len=0,$opt=false){
-        $map = [
-            'status' => ['eq',$status]
-        ];
-        $list = $this->where($map)->order('create_time desc')->limit($len,10)->select();
-        if (empty($list)){
-            return null;
-        }
-        foreach($list as $value){
-            $value['create_time'] = date('Y-m-d',$value['create_time']);
-            $Pic = Picture::where(['id' => $value['front_cover']])->find();
-            $value['front_cover'] = $Pic['name'];
-            switch($value['class']){  // 根据 class 值找对应表
-                case 1:
-                    // 获取 主图文 详情
-                    $value['pre'] = '【】';
-                    break;
-                case 2:
-                    // 获取 主图文 详情
-                    $value['pre'] = '【】';
-                    break;
-                case 3;
-                    // 获取 主图文 详情
-                    $value['pre'] = '【】';
-                    break;
-
-            }
-            // 是否 获取审核人信息
-            if ($opt){
-                // 是
-                $review = PushReview::where(['push_id' => $value['focus_main'] , 'status' => ['egt',-1]])->select();
-                foreach($review as $val){
-                    $val['create_time'] = date('Y-m-d',$val['create_time']);
-                }
-                $value['review'] = $review;
-            }
-        }
-        return $list;
     }
 }
