b2f1c3d74aa63aff954ae2b6319722050b80b577
wzc <958364865@qq.com>
2017-09-10 18:04:57 +0800

    数据统计  修改

--- application/home/controller/Statistics.php
+++ application/home/controller/Statistics.php
@@ -6,7 +6,7 @@
  * Time: 13:21
  */
 namespace app\home\controller;
-use app\home\model\WechatTest;
+use app\home\model\WechatDepartment;
 use app\home\model\WechatUser;
 
 class Statistics extends Base{
@@ -14,7 +14,30 @@ class Statistics extends Base{
      * 数据统计
      */
     public function index(){
+        //获取关注比例
+        $sum = WechatDepartment::count();     //总部门
+        $User = WechatUser::where('status',1)->select();
+        $arr = array();
+        foreach($User as $value){
+            $Depart = json_decode($value['department']);
+            if (is_array($Depart)){
+                $depart = $Depart[0];  // 两个部门 只获取第一个
+            }else{
+                $depart = $Depart;
+            }
+            $res = WechatDepartment::where('id',$depart)->find();
+            if (!empty($res) && !in_array($depart,$arr)){
+                array_push($arr,$depart);
+            }
+        }
+        $num1 = count($arr);
+        $num2 = $sum - $num1;
+        $msg = array(
+            'sum' => $sum,  // 总
+            'num_is' => $num1,  // 关注
+            'num_not' => $num2   // 未关注
+        );
+        $this->assign('msg',$msg);
         return $this->fetch();
-
     }
 }
