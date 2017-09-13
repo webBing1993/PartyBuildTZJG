d63beac9ca9e21cd9a75c2f9fc70a00a89a23499
Lxx <779219930@qq.com>
2017-09-13 15:00:34 +0800

    v1.0 志愿服务接口及交互

--- application/home/controller/Volunteer.php
+++ application/home/controller/Volunteer.php
@@ -6,17 +6,129 @@
  * Time: 13:21
  */
 namespace app\home\controller;
-use app\home\model\WechatTest;
+use app\home\model\Comment;
+use app\home\model\Like;
+use app\home\model\Picture;
 use app\home\model\WechatUser;
+use app\home\model\Volunteer as VolunteerModel;
 
 class Volunteer extends Base{
     /* 志愿服务 */
     public function index(){
+        $Model = new VolunteerModel;
+        if(IS_POST) {
+            $data = input('post.');
+            $res = $Model->getListMore($data['length'],$data['type']);
+            if($res) {
+                return $this->success("加载成功","",$res);
+            }else {
+                return $this->error("加载失败");
+            }
+        }else {
+            $list = $Model->getIndex();
+            $this->assign('list',$list);
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 发布
+     */
+    public function publish(){
+        if(IS_POST) {
+            $data = input('post.');
+            $Model = new VolunteerModel();
+            $user = WechatUser::where('userid',$data['create_user'])->find();
+            $data['publisher'] = $user['name'];
+            $data['front_cover'] = $this->default_pic(); //生成随机封面
+            isset($data["list_images"]) ? $data["list_images"] = json_encode($data["list_images"]) : $data["list_images"] = "";
+            $res = $Model->create($data);
+            if($res) {
+                return $this->success("添加成功");
+            }else {
+                return $this->error("添加失败");
+            }
+        }else {
+            $userId = session("userId");
+            $this->assign('userId',$userId);
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * @return mixed
+     * 模版1
+     */
+    public function detail() {
+        $this->anonymous();
+        $this->jssdk();
+        $Model = new VolunteerModel();
+        $id = input('id');
+        $userId = session('userId');
+        //浏览加一
+        $Model::where('id',$id)->setInc('views');
+
+        if($userId != "visitor"){
+            //浏览不存在则存入pb_browse表
+            $this->browser(8,$userId,$id);
+        }
+        //详细信息
+        $info = $Model::get($id);
+        //分享图片及链接及描述
+        $image = Picture::where('id',$info['front_cover'])->find();
+        $info['share_image'] = "http://".$_SERVER['SERVER_NAME'].$image['path'];
+        $info['link'] = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REDIRECT_URL'];
+        $info['desc'] = str_replace('&nbsp;','',strip_tags($info['content']));
+
+        //获取 文章点赞
+        $likeModel = new Like();
+        $like = $likeModel->getLike(8,$id,$userId);
+        $info['is_like'] = $like;
+        $this->assign('detail',$info);
+
+        //获取 评论
+        $commentModel = new Comment();
+        $comment = $commentModel->getComment(8,$id,$userId);
+        $this->assign('comment',$comment);
         return $this->fetch();
     }
 
-    /* 发布 */
-    public function publish(){
+    /**
+     * 模版2
+     */
+    public function detail2() {
+        $this->anonymous();
+        $this->jssdk();
+        $Model = new VolunteerModel();
+        $id = input('id');
+        $userId = session('userId');
+        //浏览加一
+        $Model::where('id',$id)->setInc('views');
+
+        if($userId != "visitor"){
+            //浏览不存在则存入pb_browse表
+            $this->browser(8,$userId,$id);
+        }
+        //详细信息
+        $info = $Model::get($id);
+        $info['images'] = json_decode($info['list_images']);
+
+        //分享图片及链接及描述
+        $image = Picture::where('id',$info['front_cover'])->find();
+        $info['share_image'] = "http://".$_SERVER['SERVER_NAME'].$image['path'];
+        $info['link'] = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REDIRECT_URL'];
+        $info['desc'] = str_replace('&nbsp;','',strip_tags($info['content']));
+
+        //获取 文章点赞
+        $likeModel = new Like();
+        $like = $likeModel->getLike(8,$id,$userId);
+        $info['is_like'] = $like;
+        $this->assign('detail',$info);
+
+        //获取 评论
+        $commentModel = new Comment();
+        $comment = $commentModel->getComment(8,$id,$userId);
+        $this->assign('comment',$comment);
         return $this->fetch();
     }
 
---
+++ application/home/model/Volunteer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lxx<779219930@qq.com>
+ * Date: 2017/9/13
+ * Time: 9:39
+ */
+
+namespace app\home\model;
+
+
+use think\Model;
+
+class Volunteer extends Model {
+    protected $insert = [
+        'views' => 0,
+        'likes' => 0,
+        'comments' => 0,
+        'create_time' => NOW_TIME,
+        'status' => 0
+    ];
+    /**
+     * 获取主页列表数据
+     */
+    public function getIndex() {
+        $map = array(
+            'type' => 1,
+            'status' => 1
+        );
+        $one = $this->where($map)->order('create_time desc')->limit(8)->select();
+
+        $map2 = array(
+            'type' => 2,
+            'status' => 1
+        );
+        $two = $this->where($map2)->order('create_time desc')->limit(8)->select();
+
+        $data = array(
+            'one' => $one,
+            'two' => $two
+        );
+        return $data;
+    }
+
+    /**
+     * @param $length
+     * @param $type
+     * @return false|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * 加载更多
+     */
+    public function getListMore($length,$type) {
+        $map = array(
+            'type' => $type,
+            'status' => 1
+        );
+        $res = $this->where($map)->order('create_time desc')->limit($length,8)->select();
+        foreach ($res as $value) {
+            $value['time'] = date("Y-m-d",$value['create_time']);
+            $path = Picture::get($value['front_cover']);
+            $value['path'] = $path['path'];
+        }
+        return $res;
+    }
+}
