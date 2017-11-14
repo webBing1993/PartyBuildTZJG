4c1ae51bde19ba73247399e316dbba5512dda615
laowang <958364865@qq.com>
2017-11-14 09:46:10 +0800

    特色创新验证  党建责任 工作计划  修改

--- application/admin/controller/Special.php
+++ application/admin/controller/Special.php
@@ -49,7 +49,7 @@ class Special extends Admin {
             $data['create_user'] = $_SESSION['think']['user_auth']['id'];
             isset($data["commend_img"]) ? $data["commend_img"] = json_encode($data["commend_img"]) : $data["commend_img"] = "";
             isset($data["voucher_img"]) ? $data["voucher_img"] = json_encode($data["voucher_img"]) : $data["voucher_img"] = "";
-            $res = $Model->create($data);
+            $res = $Model->validate(true)->save($data);
             if($res){
                 get_score(4,$res,$_SESSION['think']['user_auth']['id']);
                 return $this->success("新增成功",Url("Special/index"));
