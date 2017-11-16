765fad402d0b3674e28f26f461957497e580c687
Lxx <779219930@qq.com>
2017-11-16 16:23:28 +0800

    v1.1 图片及附件大小限制

--- application/admin/config.php
+++ application/admin/config.php
@@ -32,7 +32,7 @@ return [
     /* 图片上传相关配置 */
     'picture_upload' => array(
         'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
+        'maxSize'  => 5*1024*1024, //上传的文件大小限制 (0-不做限制)
         'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
         'autoSub'  => true, //自动子目录保存文件
         'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
@@ -48,7 +48,7 @@ return [
     /* 编辑器图片上传相关配置 */
     'editor_upload' => array(
         'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
+        'maxSize'  => 5*1024*1024, //上传的文件大小限制 (0-不做限制)
         'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
         'autoSub'  => true, //自动子目录保存文件
         'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
