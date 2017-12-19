9274e0a88888727c4c5f3c7a9f113de976bbfe8a
laowang <958364865@qq.com>
2017-12-19 15:20:45 +0800

    前台积分规则修改  修改

--- application/common.php
+++ application/common.php
@@ -212,7 +212,7 @@ function get_score($class,$aid,$userid){
                         $counts = \think\Db::name('score')->where($mapp)->whereTime('create_time','y')->count();  // 获取已发布个数
                         if ($counts < 1){
                             // 可以积分
-                            \think\Db::name('score')->insert(['class' => $class,'type' => $info['type'],'aid' => $aid,'userid' => $userid,'score_up' => 1,'score_down' => 1,'create_time' => time()]);
+                            \think\Db::name('score')->insert(['class' => $class,'type' => $type,'aid' => $aid,'userid' => $userid,'score_up' => 1,'score_down' => 1,'create_time' => time()]);
                         }
                         break;
                     case 3:  // 述职报告
